493a4383951c8e261ff32eee3b1ab0706a143840
Сергей Субботин <zarodiny@yandex.ru>
2024-10-30 14:36:51 +1000

    feat: добавляет стили

--- index.php
+++ index.php
@@ -112,97 +112,84 @@ get_header();
       <div class="w-0 min-w-[100%] relative">
         <div class="category-section-swiper swiper">
           <ul class="hero-wrapper swiper-wrapper justify-stretch">
-            <!-- 1 -->
-            <li class="swiper-slide h-auto">
-              <div class="card">
-                <div>
-                  <div class="card__sale">-20%</div>
-                  <a href="#">
-                    <div class="image-wrapper">
-                      <img src="<?php echo get_template_directory_uri() ?>/img/product/image-1.jpg" width="345"
-                        height="432" alt="product">
-                    </div>
-                  </a>
-                  <div class="card__inner">
-                    <p>DURANCE</p>
-                    <p>250 мл</p>
-                  </div>
-                  <h3 class="card__title">Рефилл DURANCE "Hazelnut cookie" Ореховое печенье
-                    с шоколадом</h3>
-                </div>
-                <div class="card__footer">
-                  <p class="card__price"><span>2 999</span> ₽</p>
-                  <a href="#" class="button">Купить</a>
-                </div>
-              </div>
-            </li>
-            <!-- 2 -->
-            <li class="swiper-slide h-auto">
-              <div class="card">
-                <div>
-                  <div class="card__sale">-90%</div>
-                  <a href="#">
-                    <div class="image-wrapper">
-                      <img src="<?php echo get_template_directory_uri() ?>/img/product/image-2.jpg" width="345"
-                        height="432" alt="product">
-                    </div>
-                  </a>
-                  <div class="card__inner">
-                    <p>CHIARA Firenze</p>
-                    <p>250 мл</p>
-                  </div>
-                  <h3 class="card__title">Наполнитель для ароматического диффузора "TUSCIA" Вековая смола...</h3>
-                </div>
-                <div class="card__footer">
-                  <p class="card__price"><span>3 490</span> ₽</p>
-                  <a href="#" class="button">Купить</a>
-                </div>
-              </div>
-            </li>
-            <!-- 3 -->
-            <li class="swiper-slide h-auto">
-              <div class="card">
-                <div>
-                  <a href="#">
-                    <div class="image-wrapper">
-                      <img src="<?php echo get_template_directory_uri() ?>/img/product/image-3.jpg" width="345"
-                        height="432" alt="product">
-                    </div>
-                  </a>
-                  <div class="card__inner">
-                    <p>Culti Milano</p>
-                    <p>250 мл</p>
-                  </div>
-                  <h3 class="card__title">Рефилл для заправки диффузора CULTI Quercea 1000 мл.</h3>
-                </div>
-                <div class="card__footer">
-                  <p class="card__price"><span>13 490</span> ₽</p>
-                  <a href="#" class="button">Купить</a>
-                </div>
-              </div>
-            </li>
-            <!-- 4 -->
-            <li class="swiper-slide h-auto">
-              <div class="card">
-                <div>
-                  <a href="#">
-                    <div class="image-wrapper">
-                      <img src="<?php echo get_template_directory_uri() ?>/img/product/image-4.jpg" width="345"
-                        height="432" alt="product">
-                    </div>
-                  </a>
-                  <div class="card__inner">
-                    <p>Voluspa</p>
-                    <p>250 мл</p>
-                  </div>
-                  <h3 class="card__title">Ароматический диффузор
-                    100 мл PANJORE LYCHEE "Панжерское личи"</h3>
-                </div>
-                <div class="card__footer">
-                  <p class="card__price"><span>4 490</span> ₽</p>
-                  <a href="#" class="button">Купить</a>
-                </div>
-              </div>
+            <?php
+            $args = array(
+              'post_type' => 'product',
+              'posts_per_page' => -1,
+              'product_cat' => 'new', // Замените 'news' на слаг вашей категории
+            );
+
+            $loop = new WP_Query($args);
+
+            if ($loop->have_posts()) {
+              while ($loop->have_posts()):
+                $loop->the_post();
+                global $product;
+                $product_id = get_the_ID();
+
+                // Получаем атрибуты товара
+                $attr = $product->get_attributes(); // Получаем весь массив с атрибутами товаров
+            
+                $array = array(); // Создаем пустой массив
+                foreach ($attr as $key => $value) {
+                  $array[$key] = (($value->get_terms())[0]);
+                }
+
+                echo '<li class="swiper-slide h-auto product type-product status-publish purchasable woocommerce">';
+                echo ' <div class="card">';
+                echo ' <div>';
+                if ($product->sale_price) {
+                  echo '<div class="card__sale">-' . (100 - round((int) $product->price * 100 / (int) $product->regular_price)) . '%</div>';
+                }
+
+                echo '<a href="' . get_permalink($loop->post->ID) . '">';
+                echo ' <div class="image-wrapper">';
+                if (has_post_thumbnail($loop->post->ID)) {
+                  echo get_the_post_thumbnail($loop->post->ID, 'shop_catalog');
+                } else {
+                  echo '<img loading="lazy" src="' . get_template_directory_uri() . '/img/product/image-1.jpg" width="345"
+                          height="432" alt="product" alt="">';
+                }
+                echo '</div>';
+                echo '</a>';
+                echo ' <div class="card__inner">';
+
+                if (isset($array['pa_brend'])) {
+                  $brend = $array['pa_brend']->name;
+                  echo '<p>' . $brend . '</p>';
+                }
+
+                if (isset($array['pa_obvem'])) {
+                  $obvem = $array['pa_obvem']->name;
+                  echo '<p>' . $obvem . '</p>';
+                }
+
+                echo ' </div>';
+                echo '<h3 class="card__title">' . esc_html($loop->post->post_title) . '</h3>';
+                echo ' </div>';
+                echo ' <div class="card__footer">';
+
+                echo '<p class="card__price">';
+
+                echo '<span>' . number_format((int) $product->price, 0, '.', ' ') . '&nbsp;₽</span>';
+                if ($product->sale_price) {
+                  echo '<span class="sale-price">' . number_format((int) $product->regular_price, 0, '.', ' ') . '&nbsp;₽</span>';
+                }
+                echo '</p>';
+
+                echo ' <a href="?add-to-cart=' . $product_id . '" class="button product_type_simple add_to_cart_button ajax_add_to_cart"
+                data-quantity="1" data-product_id="' . $product_id . '" data-product_sku="' . $product->get_sku() . '" aria-label="' . __('Добавить в корзину', 'domain') . '" rel="nofollow">Купить</a>';
+                echo ' </div>';
+                echo '</div>';
+                echo '</li>';
+
+              endwhile;
+            } else {
+              echo __('Товаров не найдено');
+            }
+
+            wp_reset_postdata();
+            ?>
             </li>
           </ul>
         </div>
@@ -236,7 +223,7 @@ get_header();
                         height="432" alt="product">
                     </div>
                   </a>
-                  <div class="card__inner">
+                  <div class=" card__inner">
                     <p>DURANCE</p>
                     <p>250 мл</p>
                   </div>
@@ -260,7 +247,7 @@ get_header();
                         height="432" alt="product">
                     </div>
                   </a>
-                  <div class="card__inner">
+                  <div class=" card__inner">
                     <p>CHIARA Firenze</p>
                     <p>250 мл</p>
                   </div>
@@ -282,7 +269,7 @@ get_header();
                         height="432" alt="product">
                     </div>
                   </a>
-                  <div class="card__inner">
+                  <div class=" card__inner">
                     <p>Culti Milano</p>
                     <p>250 мл</p>
                   </div>
@@ -304,7 +291,7 @@ get_header();
                         height="432" alt="product">
                     </div>
                   </a>
-                  <div class="card__inner">
+                  <div class=" card__inner">
                     <p>Voluspa</p>
                     <p>250 мл</p>
                   </div>
@@ -343,7 +330,8 @@ get_header();
               <a href="">
                 <img src="<?php echo get_template_directory_uri() ?>/img/brands/image-1.png" width="177" height="177"
                   alt="image">
-                <div class="brands-mask">Atmosphera</div>
+                <div class=" brands-mask">Atmosphera
+                </div>
               </a>
             </li>
             <!-- 2 -->
@@ -351,7 +339,8 @@ get_header();
               <a href="">
                 <img src="<?php echo get_template_directory_uri() ?>/img/brands/image-1.png" width="177" height="177"
                   alt="image">
-                <div class="brands-mask">Atmosphera</div>
+                <div class=" brands-mask">Atmosphera
+                </div>
               </a>
             </li>
             <!-- 3 -->
@@ -359,7 +348,8 @@ get_header();
               <a href="">
                 <img src="<?php echo get_template_directory_uri() ?>/img/brands/image-1.png" width="177" height="177"
                   alt="image">
-                <div class="brands-mask">Atmosphera</div>
+                <div class=" brands-mask">Atmosphera
+                </div>
               </a>
             </li>
             <!-- 4 -->
@@ -367,7 +357,7 @@ get_header();
               <a href="">
                 <img src="<?php echo get_template_directory_uri() ?>/img/brands/image-1.png" width="177" height="177"
                   alt="image">
-                <div class="brands-mask">Atmosphera</div>
+                <div class=" brands-mask">Atmosphera</div>
               </a>
             </li>
             <!-- 5 -->
@@ -375,7 +365,7 @@ get_header();
               <a href="">
                 <img src="<?php echo get_template_directory_uri() ?>/img/brands/image-1.png" width="177" height="177"
                   alt="image">
-                <div class="brands-mask">Atmosphera</div>
+                <div class=" brands-mask">Atmosphera</div>
               </a>
             </li>
             <!-- 6 -->
@@ -383,7 +373,7 @@ get_header();
               <a href="">
                 <img src="<?php echo get_template_directory_uri() ?>/img/brands/image-1.png" width="177" height="177"
                   alt="image">
-                <div class="brands-mask">Atmosphera</div>
+                <div class=" brands-mask">Atmosphera</div>
               </a>
             </li>
           </ul>
@@ -419,7 +409,7 @@ get_header();
             <img src="<?php echo get_template_directory_uri() ?>/img/icons/pin-white.svg" width="20" height="20"
               alt="pin">
           </div>
-          <p class="contacts__text">г. Находка, ул. ​Малиновского, 19а</p>
+          <p class=" contacts__text">г. Находка, ул. ​Малиновского, 19а</p>
         </div>
 
         <div class="flex items-center gap-3 contacts__header-item">
@@ -427,7 +417,7 @@ get_header();
             <img src="<?php echo get_template_directory_uri() ?>/img/icons/phone-white.svg" width="20" height="20"
               alt="pin">
           </div>
-          <a class="contacts__text" href="tel:+79999999999">+ 7 (999) 999-99-99</a>
+          <a class=" contacts__text" href="tel:+79999999999">+ 7 (999) 999-99-99</a>
         </div>
 
         <div class="flex items-center gap-3 contacts__header-item">
@@ -446,7 +436,7 @@ get_header();
               alt="pin">
           </div>
           <div>
-            <a class="contacts__slim-text" href="https://wa.me/+79999999999">Написать на WhatsApp</a>
+            <a class=" contacts__slim-text" href="https://wa.me/+79999999999">Написать на WhatsApp</a>
           </div>
         </div>
 
